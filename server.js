36b741636bc087d82db48e7aa044452dfd2839c5
Cai Chengyuan (XLD5QFR) <ccai41@gatech.edu>
2017-06-20 12:28:44 -0400

    kevin

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
+// Get dependencies
 const express = require('express');
-const app = express();
 const path = require('path');
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+/**
+* Get port from environment and store in Express.
+*/
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+* Create HTTP server.
+*/
+const server = http.createServer(app);
+
+/**
+* Listen on provided port, on all network interfaces.
+*/
+server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+
