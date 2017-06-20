7bea3bcc9eb706dab87a4a0dc0516959ca68732f
Cai Chengyuan (XLD5QFR) <ccai41@gatech.edu>
2017-06-20 12:03:24 -0400

    change server.js

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
