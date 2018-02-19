88ba6d00022d9f6976c6e1bb2481de4e8a1fcc36
ChengyuanCai <kevin.c.cai@gatech.edu>
2018-02-18 20:48:46 -0500

    change nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ import {ResumeComponent} from './resume/resume.component';
 export const rootRouterConfig: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'project', component: AboutComponent },
   { path: 'github', component: RepoBrowserComponent,
     children: [
       { path: '', component: RepoListComponent },
